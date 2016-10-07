21b3837c4f07dad85ecc889f644d492b41c11270
Ian Meintjes <meintjes@umich.edu>
2016-10-06 23:37:02 -0400

    Fix trackbar code
    Refactored trackbar code, made only one window show up, and made HSV
    colors show up normally.

--- trackbars.py
+++ trackbars.py
@@ -5,15 +5,15 @@ import sys
 def nothing(x):
     pass
 
-
 #defaults to RBG trackbars
 #rgb for rgb trackbars, hsv for hsv trackbars
 def main(argv):
-
     #Capture video from the stream
     cap = cv2.VideoCapture(0)
-    cv2.namedWindow('Colorbars') #Create a window named 'Colorbars'
-    wnd = 'Track Bars'
+
+    #Create a window named 'Colorbars'
+    wnd = 'Colorbars'
+    cv2.namedWindow(wnd)
 
     if len(argv) == 0 or argv[0] == 'rgb':
         rl = 'Red Low'
@@ -22,20 +22,9 @@ def main(argv):
         gh = 'Green High'
         bl = 'Blue Low'
         bh = 'Blue High'
-        
-
         bars = [rl, rh, gl, gh, bl, bh]
 
-        #begin creating trackbars for rgb
-        cv2.createTrackbar(rl, wnd,0,255,nothing)
-        cv2.createTrackbar(rh, wnd,0,255,nothing)
-        cv2.createTrackbar(gl, wnd,0,255,nothing)
-        cv2.createTrackbar(gh, wnd,0,255,nothing)
-        cv2.createTrackbar(bl, wnd,0,255,nothing)
-        cv2.createTrackbar(bh, wnd,0,255,nothing)
-
     elif len(argv) > 0 and argv[0] == 'hsv':
-        
         #assign strings for ease of coding
         hh='Hue High'
         hl='Hue Low'
@@ -45,29 +34,21 @@ def main(argv):
         vl='Value Low'
         bars = [hl,hh,sl,sh,vl,vh]
 
-        #Begin Creating trackbars for hsv
-        cv2.createTrackbar(hl, wnd,0,179,nothing)
-        cv2.createTrackbar(hh, wnd,0,179,nothing)
-        cv2.createTrackbar(sl, wnd,0,255,nothing)
-        cv2.createTrackbar(sh, wnd,0,255,nothing)
-        cv2.createTrackbar(vl, wnd,0,255,nothing)
-        cv2.createTrackbar(vh, wnd,0,255,nothing)
-
     else:
-        print 'stop trying to break the program\n \
-                insert nothing for rgb, \'rgb\' for \
-                rgb or \'hsv\' for hsv!'
+        print '''insert nothing for rgb, 'rgb' for rgb or 'hsv' for hsv'''
+        return
+
+    for bar in bars:
+        cv2.createTrackbar(bar, wnd, 0, 255, nothing)
+
 
     #begin our 'infinite' while loop
     while(1):
         #read the streamed frames (we previously named this cap)
         _,frame=cap.read()
-     
+
         #it is common to apply a blur to the frame
         frame=cv2.GaussianBlur(frame,(5,5),0)
-     
-        #convert from a BGR stream to an HSV stream
-        hsv=cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
         bar1low=cv2.getTrackbarPos(bars[0],wnd)
         bar1high=cv2.getTrackbarPos(bars[1],wnd)
@@ -80,23 +61,26 @@ def main(argv):
         rangehigh = np.array([bar1high,bar2high,bar3high])
 
         #create a mask for that range
-        if len(argv) > 1 and argv[0] == 'hsv':
+        if len(argv) > 0 and argv[0] == 'hsv':
+            #convert from a BGR stream to an HSV stream
+            hsv=cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
             mask = cv2.inRange(hsv,rangelow, rangehigh)
+            hsv_res = cv2.bitwise_and(hsv,hsv, mask =mask)
+
+            #convert back to HSV so it shows up all normal like
+            hsv_res=cv2.cvtColor(hsv_res, cv2.COLOR_HSV2BGR)
+            cv2.imshow("hsv", hsv_res)
+
         else:
             mask = cv2.inRange(frame,rangelow,rangehigh)
+            res = cv2.bitwise_and(frame,frame, mask =mask)
+            cv2.imshow("rgb", res)
 
-
-        res = cv2.bitwise_and(frame,frame, mask =mask)
-        hsv_res = cv2.bitwise_and(hsv,hsv, mask =mask)
-     
-        cv2.imshow(wnd, res)
-        cv2.imshow("hsv", hsv_res)
         k = cv2.waitKey(5) & 0xFF
         if k == ord('q'):
             break
-     
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-    
