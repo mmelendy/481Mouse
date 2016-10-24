aa9367e259dae27c6d67f4ebe595c545482a885e
Ian Meintjes <meintjes@umich.edu>
2016-10-23 20:13:53 -0400

    clean up camera code and apply erosion/dilation to mask

--- camera.py
+++ camera.py
@@ -5,7 +5,7 @@ import numpy as np
 from collections import deque
 
 import colors
-from interface import ColorApp
+#from interface import ColorApp
 from mouse import BasicController
 
 def main(argv): 
@@ -15,18 +15,18 @@ def main(argv):
     #get_camera_values(cap)
 
     parser = argparse.ArgumentParser(description='HSV Color Space of a Single Color')
-    #parser.add_argument("color", help="choose common color to start, bad color defaults blue")
+    parser.add_argument("color", help="choose common color to start, bad color defaults blue")
     parser.add_argument("--circle", "-c", help="draw circle around object", action="store_true")
     parser.add_argument("--bars", "-b", help="add trackbars for erosion and dilation", action="store_true")
     args = parser.parse_args()
-    #color = set_color(args.color)
+    color = set_color(args.color)
     
 
     #app = ColorApp()
     #app.MainLoop()
 
 
-    print app.frame.curr_color
+    #print app.frame.curr_color
     sys.stdout.flush()
 
     pts = deque(maxlen=32)
@@ -34,21 +34,19 @@ def main(argv):
     frame_width = cap.get(3)
     frame_height = cap.get(4)
     mouse = BasicController()
-    mouse.margin = (0.1, 0.3, 0.15, 0.15)
-
-    if args.bars:
-        cv2.namedWindow("mask")
-        cv2.createTrackbar("Erosion", "mask", 0, 10, nothing)
-        cv2.createTrackbar("Dilation", "mask", 0, 10, nothing)
+    mouse.margin = (0.1, 0.2, 0.15, 0.15)
 
     ero_it = 2
-    dil_it = 3
+    dil_it = 1
+    if args.bars:
+        cv2.namedWindow("mask")
+        cv2.createTrackbar("Erosion", "mask", ero_it, 10, nothing)
+        cv2.createTrackbar("Dilation", "mask", dil_it, 10, nothing)
 
     while True:
         ret, img = cap.read()
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-        #if 
         if args.color != 'red':
             mask = cv2.inRange(hsv, color[0], color[1])
 
@@ -58,23 +56,16 @@ def main(argv):
             mask1 = cv2.inRange(hsv, color[1][0], color[1][1])
             mask = mask0 + mask1
 
-        #get trackbar positions, iterations cant be 0
+        #get trackbar positions
         if args.bars:
             ero_it = cv2.getTrackbarPos("Erosion", "mask")
-            if ero_it == 0:
-                ero_it = 1
             dil_it = cv2.getTrackbarPos("Dilation", "mask")
-            if dil_it == 0:
-                dil_it = 1
 
-        #opening = cv2.morphologyEx(res, cv2.MORPH_OPEN, kernel)
-        erode = cv2.erode(mask,kernel,iterations = ero_it)
-        dil = cv2.dilate(mask,kernel,iterations = dil_it)
-        opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+        mask = cv2.erode(mask,kernel,iterations = ero_it)
+        mask = cv2.dilate(mask,kernel,iterations = dil_it)
 
         contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
                                      cv2.CHAIN_APPROX_SIMPLE)[-2]
-        
         if len(contours) > 0:
             max_con = max(contours, key=cv2.contourArea)
             ((x,y), radius) = cv2.minEnclosingCircle(max_con)
@@ -88,6 +79,7 @@ def main(argv):
             if radius > 10 and args.circle:
                 cv2.circle(img, (int(x), int(y)), int(radius), (255,0,0), 2)
                 cv2.circle(img, center, 5,(0,0,255), -1)
+                cv2.circle(mask, (int(x), int(y)), int(radius), (255,0,0), 2)
                 pts.appendleft(center)
 
             #mouse movement
@@ -95,16 +87,8 @@ def main(argv):
             y = center[1] / frame_height
             mouse.move(x, y)
 
+        cv2.imshow("mask", mask)
         cv2.imshow("input", img)
-
-        #list of various views
-        #cv2.imshow("opening", opening)
-        #cv2.imshow("mask", mask)
-        #cv2.imshow("dil", dil)
-        #cv2.imshow("erode", erode)
-        #cv2.imshow("erosion and dilation", morph)
-        #res = cv2.bitwise_and(img, img, mask= mask)
-        #cv2.imshow("res " + args.color, res)
 
         key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
